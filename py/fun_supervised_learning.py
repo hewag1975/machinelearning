8b65fb266735e78575ba5b118fb11bb22f5aa616
Hendrik Wagenseil <hendrik.wagenseil@addium.io>
2024-01-05 15:43:52 +0100

    init fun collection for supervised learning

---
+++ py/fun_supervised_learning.py
@@ -0,0 +1,88 @@
+
+import numpy as np
+
+def getlossMSE(y: float, yhat: float):
+  return (yhat - y) ** 2
+
+def getcost(x: np.ndarray, y: np.ndarray, w: np.ndarray, b: float = 0): 
+  
+  m = x.shape[0]
+  yhat = np.dot(x, w) + b
+  loss = getlossMSE(y, yhat = yhat)
+  cost = sum(loss)
+  cost = (1 / (2 * m)) * cost
+
+  return cost
+  
+def gradient(x, y, w, b):
+    m = x.shape[0]    
+
+    yhat = np.dot(x, w) + b
+    diff = (yhat - y)
+    
+    dj_dw = x * diff[:, None]
+    dj_db = diff
+    
+    dj_dw = np.sum(dj_dw, axis = 0) / m
+    dj_db = np.sum(dj_db, axis = 0) / m
+    
+    return dj_dw, dj_db
+
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function): 
+    """
+    Performs gradient descent to fit w,b. Updates w,b by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      x (ndarray (m,))  : Data, m examples 
+      y (ndarray (m,))  : target values
+      w_in,b_in (scalar): initial values of model parameters  
+      alpha (float):     Learning rate
+      num_iters (int):   number of iterations to run gradient descent
+      cost_function:     function to call to produce cost
+      gradient_function: function to call to produce gradient
+      
+    Returns:
+      w (scalar): Updated value of parameter after running gradient descent
+      b (scalar): Updated value of parameter after running gradient descent
+      J_history (List): History of cost values
+      p_history (list): History of parameters [w,b] 
+      """
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    p_history = []
+    b = b_in
+    w = w_in
+    
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters using gradient_function
+        dj_dw, dj_db = gradient_function(x, y, w , b)     
+
+        # Update Parameters using equation (3) above
+        b = b - alpha * dj_db                            
+        w = w - alpha * dj_dw                            
+
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( cost_function(x, y, w , b))
+            p_history.append([w,b])
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            print(f"Iteration {i:4}: Cost {J_history[-1]:0.2e} ",
+                  f"dj_dw: {dj_dw: 0.3e}, dj_db: {dj_db: 0.3e}  ",
+                  f"w: {w: 0.3e}, b:{b: 0.5e}")
+ 
+    return w, b, J_history, p_history 
+
+
+  
+x = np.random.uniform(size = 1000)
+x = x.reshape((200, 5))
+
+y = np.random.uniform(size = 200)
+
+w = np.random.normal(size = 5)
+b = 2.5
+getcost(x, y, w, b)
+w.shape
